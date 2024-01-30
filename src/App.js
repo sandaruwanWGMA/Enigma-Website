21b573c803ac0119cebf0335c8becdd1d106f237
SandaruwanWGMA <molinduachintha24@gmail.com>
2024-01-30 08:58:21 +0530

    edited bg image

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import home_bg_img from "./assets/home_bg_img.png";
+import home_bg_img from "./assets/home_bg_img.jpeg";
 import enigmaLogo from "./assets/enigma_logo.svg";
 import "./App.css";
 import CountdownTimer from "./components/CountdownTimer";
@@ -7,6 +7,7 @@ import CountdownTimer from "./components/CountdownTimer";
 const App = () => {
   return (
     <section id="home" className="home-section">
+      <div className="image-overlay"></div>
       <img src={home_bg_img} alt="home-bg-image" className="home-bg-img" />
       <img src={enigmaLogo} alt="enigma-logo" className="enigma-logo" />
       <CountdownTimer
