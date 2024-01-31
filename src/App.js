a694d1cc8c3837b030e9868784dbfa7a3717fb7b
SandaruwanWGMA <molinduachintha24@gmail.com>
2024-01-31 08:35:46 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
 import HomeSection from "./Sections/Home Section/HomeSection";
+import AboutSection from "./Sections/About Section/AboutSection";
 
 const App = () => {
-  return <HomeSection></HomeSection>;
+  return (
+    <>
+      <HomeSection></HomeSection>
+      <AboutSection></AboutSection>
+    </>
+  );
 };
 export default App;
