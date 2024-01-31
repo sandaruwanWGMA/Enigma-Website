be52640ec080998897b0635e081291e524e3d741
SandaruwanWGMA <molinduachintha24@gmail.com>
2024-01-31 08:51:46 +0530

    made the stucrure for about

--- src/Sections/About Section/AboutSection.jsx
+++ src/Sections/About Section/AboutSection.jsx
@@ -4,8 +4,36 @@ import "./About Section Styles/AboutSectionStyles.css";
 
 function AboutSection(props) {
   return (
-    <section>
+    <section id="about">
       <img src={about_bg_img} alt="about bg image" className="about-bg-img" />
+      <div>
+        <p>
+          Enigma is an intra-university coding competition organized by
+          MoraMaths, consisting of
+        </p>
+        <ul>
+          <li>
+            A six-week workshop series, focusing on Python Programming and
+            Mathematics, aimed specifically at the 23rd Batch of University of
+            Moratuwa (but open to all undergraduates), and
+          </li>
+          <li>
+            A hackathon comprising two rounds, open to all undergraduates within
+            the university.
+          </li>
+        </ul>
+        <p>
+          Taking part in Enigma will not only leave you with strong skills in
+          both mathematics and programming, but also offer you a view of
+          mathematics from a different, previously unseen perspective, further
+          igniting your passion for the subject.
+        </p>
+        <p>
+          This will be an excellent opportunity for you to improve your
+          problem-solving skills and gain a deeper understanding of mathematics
+          and programming, and of course, have fun with your colleagues!
+        </p>
+      </div>
     </section>
   );
 }
