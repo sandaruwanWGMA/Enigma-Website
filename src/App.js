9ed0d477e8049bcf99291fe93b4289b31953d79b
SandaruwanWGMA <molinduachintha24@gmail.com>
2024-01-31 16:04:18 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import Accordition from "./Sections/FAQ/Accordition";
 import HomeSection from "./Sections/Home Section/HomeSection";
 import AboutSection from "./Sections/About Section/AboutSection";
@@ -8,13 +7,13 @@ import Guidelines from "./Sections/Guidelines/Guidelines.jsx";
 import EventTimeline from "./Sections/EventTimeline/EventTimeline.jsx";
 
 const App = () => {
-  return <Accordition />;
   return (
     <>
       <HomeSection></HomeSection>
       <AboutSection></AboutSection>
       <EventTimeline></EventTimeline>
       <Guidelines></Guidelines>
+      <Accordition />
     </>
   );
 };
