9f87fb20602470b31487ff6ef6b1b8a59d74b038
Pasindu Sankalpa <105929988+Pasindu599@users.noreply.github.com>
2024-02-01 10:43:49 +0530

    AwardSection component added to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import HomeSection from "./Sections/Home Section/HomeSection";
 import AboutSection from "./Sections/About Section/AboutSection";
+import AwardSection from "./Sections/AwardSection/AwardSection";
 
 const App = () => {
   return (
     <>
       <HomeSection></HomeSection>
       <AboutSection></AboutSection>
+      <AwardSection></AwardSection>
     </>
   );
 };
