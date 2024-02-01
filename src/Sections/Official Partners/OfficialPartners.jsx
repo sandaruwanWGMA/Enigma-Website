098ddddba322c472e5aa53c0181b42629701116e
SandaruwanWGMA <molinduachintha24@gmail.com>
2024-02-01 10:15:05 +0530

    commit

--- src/Sections/Official Partners/OfficialPartners.jsx
+++ src/Sections/Official Partners/OfficialPartners.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ReactComponent as BGImage } from "../../assets/official-partners-page/vintage-world-war.svg";
+import world_war from "../../assets/official-partners-page/vintage-world-war.svg";
 import rec from "../../assets/official-partners-page/Rectangle 3.png";
 import army_man from "../../assets/official-partners-page/Rectangle 4.png";
 import dialog from "../../assets/official-partners-page/dialog logo.svg";
@@ -11,7 +12,8 @@ import "./Official Partners Styles/OfficialParterns.css";
 function OfficialPartners(props) {
   return (
     <section className="official-partners-section">
-      <BGImage className="bg-world-war" />
+      <img src={world_war} className="bg-world-war"></img>
+      {/* <BGImage className="bg-world-war" /> */}
       <img src={rec} alt="rectangle" className="rec"></img>
       <img src={army_man} alt="army man" className="army-man"></img>
       <div className="logos">
