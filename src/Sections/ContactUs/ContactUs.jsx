5c0a460a344dc843d2cc91c0ac83d1222ebefdbd
Verosha Kriyanjala <veroshakriyanjala32@gmail.com>
2024-02-05 20:16:16 +0530

    Contact us js and css completed

--- src/Sections/ContactUs/ContactUs.jsx
+++ src/Sections/ContactUs/ContactUs.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ContactUsBackgroungImg from "../../assets/ContactUs/ContactUs.svg";
+import ContactUsBackgroungMobile from "../../assets/ContactUs/ContactUsMobile.svg";
 import "./ContactUsStyles/ContactUs.css";
 
 function ContactUs(props) {
@@ -9,6 +10,16 @@ function ContactUs(props) {
         <img src={ContactUsBackgroungImg} alt="" className="contactUs" />
         <div className="ContactUsTopic">
           <h1 id="contactUsTopic">Contact Us</h1>
+        </div>
+      </div>
+      <div className="ContactUsMobile">
+        <img
+          src={ContactUsBackgroungMobile}
+          alt=""
+          className="contactUsMobile"
+        />
+        <div className="ContactUsTopicMobile">
+          <h1 id="contactUsTopicMobile">Contact Us</h1>
         </div>
       </div>
     </section>
