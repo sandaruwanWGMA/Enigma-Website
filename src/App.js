a2da258b3a98a3a567f4eeb08608dcfe3e108175
Verosha Kriyanjala <veroshakriyanjala32@gmail.com>
2024-02-05 12:00:48 +0530

    edited app.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import "./App.css";
 import Guidelines from "./Sections/Guidelines/Guidelines.jsx";
 import EventTimeline from "./Sections/EventTimeline/EventTimeline.jsx";
+import ContactUs from "./Sections/ContactUs/ContactUs.jsx";
 
 const App = () => {
   return (
     <section>
       <EventTimeline></EventTimeline>
       <Guidelines></Guidelines>
+      <ContactUs></ContactUs>
     </section>
   );
 };
+
 export default App;
