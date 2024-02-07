a2d965aeb03cf8b6b4b1df3740994a705d97e234
SandaruwanWGMA <molinduachintha24@gmail.com>
2024-02-07 19:32:03 +0530

    commit

--- src/App-Dell-Inspiron.js
+++ src/App-Dell-Inspiron.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import HomeSection from "./Sections/HomeSection/HomeSection.jsx";
 import Guidelines from "./Sections/Guidelines/Guidelines.jsx";
 import EventTimeline from "./Sections/EventTimeline/EventTimeline.jsx";
 
 const App = () => {
   return (
     <section>
+      <HomeSection></HomeSection>
       <EventTimeline></EventTimeline>
       <Guidelines></Guidelines>
     </section>
--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import HomeSection from "./Sections/Home Section/HomeSection.jsx";
 import Guidelines from "./Sections/Guidelines/Guidelines.jsx";
 import EventTimeline from "./Sections/EventTimeline/EventTimeline.jsx";
 import ContactUs from "./Sections/ContactUs/ContactUs.jsx";
@@ -7,6 +8,7 @@ import ContactUs from "./Sections/ContactUs/ContactUs.jsx";
 const App = () => {
   return (
     <>
+      <HomeSection></HomeSection>
       <EventTimeline></EventTimeline>
       <Guidelines></Guidelines>
       <ContactUs></ContactUs>
