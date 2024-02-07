77b5cac75e7e918282ff84ae4dac019b99667225
SandaruwanWGMA <molinduachintha24@gmail.com>
2024-02-07 19:30:08 +0530

    home and about

---
+++ src/Sections/About Section/AboutSection.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import about_bg_img from "../../assets/Images-about-page/about bg image.png";
+import about_bg_img_8_plus from "../../assets/Images-about-page/about-bg-8-plus.jpeg";
+import floating_img from "../../assets/Images-about-page/about floating image.png";
+import "./About Section Styles/AboutSectionStyles.css";
+
+function AboutSection(props) {
+  return (
+    <section id="about">
+      <img src={about_bg_img} alt="about bg image" className="about-bg-img" />
+      <img
+        src={about_bg_img_8_plus}
+        alt="about bg image2"
+        className="about-bg-img-8-plus"
+      />
+      <div className="img-overlay"></div>
+      <div className="content">
+        <div className="title">
+          <p className="title-text">WHAT IS ENIGMA</p>
+        </div>
+        <div className="description">
+          <p>
+            ENIGMA ‘24 is a novel initiative pioneered by the Mathematics
+            Society of the University of Moratuwa, that comprises an
+            intra-university programming competition, open to all undergraduates
+            of the university.
+          </p>
+          {/* <ul>
+            <li>
+              A six-week workshop series, focusing on Python Programming and
+              Mathematics, aimed specifically at the 23rd Batch of University of
+              Moratuwa (but open to all undergraduates), and
+            </li>
+            <li>
+              A hackathon comprising two rounds, open to all undergraduates
+              within the university.
+            </li>
+          </ul> */}
+          <p>
+            This stands as an unparalleled opportunity for participants to
+            confront programming challenges woven intricately into mathematics,
+            providing them an opportunity to deepen their knowledge of both
+            subjects at the same time.
+          </p>
+          <p>
+            ENIGMA ‘24 will set its participants on an exciting journey, where
+            they will refine their programming skills, enhance their
+            problem-solving skills through the application of mathematical
+            concepts, and foster their teamwork skills, all of which are
+            integral competencies required in today’s competitive landscape.
+          </p>
+        </div>
+      </div>
+      <img
+        src={floating_img}
+        alt="floating image"
+        className="floating-img"
+      ></img>
+    </section>
+  );
+}
+
+export default AboutSection;
---
+++ src/Sections/Home Section/Home Section Components/NabBar.jsx
@@ -0,0 +1,109 @@
+import React, { useState } from "react";
+import "../Home Section Styles/NavBar.css";
+import "../Home Section Styles/HorizontalLine.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import { ReactComponent as LinkedInIcon } from "../../../assets/Images-home-page/linkedin-icon.svg";
+import { ReactComponent as FacebookIcon } from "../../../assets/Images-home-page/facebook-icon.svg";
+import { ReactComponent as InstagramIcon } from "../../../assets/Images-home-page/instagram-icon.svg";
+
+const NavBar = ({ style, scrollToAbout }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const flipIsOpen = () => {
+    console.log(isOpen);
+    setIsOpen(!isOpen);
+  };
+  return (
+    <>
+      <button className="toggle-btn" onClick={flipIsOpen}>
+        <p>{isOpen ? "╳" : "☰"}</p>
+      </button>
+      <nav className={isOpen ? `mobile` : `mobile-not`}>
+        <p className="titile-nav-mob">ENIGMA ˈ24</p>
+        <a href="#home" className="nav-links">
+          Home
+        </a>
+        <a href="#about" className="nav-links">
+          About
+        </a>
+        <a href="#timeline" className="nav-links">
+          Timeline
+        </a>
+        <a href="#guidelines" className="nav-links">
+          Guidelines
+        </a>
+        <a href="#awards" className="nav-links">
+          Awards
+        </a>
+        <a href="#contact" className="nav-links">
+          Contact Us
+        </a>
+        <a href="#faq" className="nav-links">
+          FAQ
+        </a>
+        {/* <a onClick={scrollToAbout} href="#more" className="nav-links">
+          More{" "}
+          <FontAwesomeIcon icon={faChevronDown} style={{ fontSize: "0.9em" }} />
+        </a> */}
+        <div className="bottom-box">
+          <div className="horizontal-line"></div>
+          <div className="social-icons-mob">
+            <a
+              href="https://www.linkedin.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="linkedin-icon"
+            >
+              <LinkedInIcon className="linkedin-icon icon-mob" />
+            </a>
+            <a
+              href="https://www.facebook.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="facebook-icon"
+            >
+              <FacebookIcon className="facebook-icon icon-mob" />
+            </a>
+            <a
+              href="https://www.instagram.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="instagram-icon"
+            >
+              <InstagramIcon className="instagram-icon icon-mob" />
+            </a>
+          </div>
+        </div>
+      </nav>
+      <nav style={style} className={isOpen ? "open-m" : "navigation"}>
+        <a href="#home" className="nav-links">
+          Home
+        </a>
+        <a href="#about" className="nav-links">
+          About
+        </a>
+        <a href="#timeline" className="nav-links">
+          Timeline
+        </a>
+        <a href="#guidelines" className="nav-links">
+          Guidelines
+        </a>
+        <a href="#awards" className="nav-links">
+          Awards
+        </a>
+        <a href="#contact" className="nav-links">
+          Contact Us
+        </a>
+        <a href="#faq" className="nav-links">
+          FAQ
+        </a>
+        {/* <a onClick={scrollToAbout} href="#more">
+          More{" "}
+          <FontAwesomeIcon icon={faChevronDown} style={{ fontSize: "0.9em" }} />
+        </a> */}
+      </nav>
+    </>
+  );
+};
+
+export default NavBar;
