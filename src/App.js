b1a8d228a09e118f1469fb2afc464ff0befb5e9c
SandaruwanWGMA <molinduachintha24@gmail.com>
2024-02-07 20:24:44 +0530

    c

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import FullPageScroll from "./FullPageScrolling";
 import AwardSection from "./Sections/AwardSection/AwardSection";
+import Accordition from "./Sections/FAQ/Accordition";
 
 const App = () => {
-  return <AwardSection></AwardSection>;
+  return <Accordition></Accordition>;
 };
 export default App;
