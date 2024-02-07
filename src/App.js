9f4beefd9e56074dc9496f3d00d91ef70415c0f0
SandaruwanWGMA <molinduachintha24@gmail.com>
2024-02-07 08:48:08 +0530

    responsive foe 6s

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import HomeSection from "./Sections/Home Section/HomeSection";
 import AboutSection from "./Sections/About Section/AboutSection";
-import NavMobile from "./Sections/Home Section/Nav Mobile View/NavMobile";
 
 const App = () => {
   return (
