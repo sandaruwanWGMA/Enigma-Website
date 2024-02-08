81552dbe395ef7ed614e21360ac393b317d0b74f
SandaruwanWGMA <molinduachintha24@gmail.com>
2024-02-08 13:45:49 +0530

    done my part

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
 import React from "react";
 import "./App.css";
-import HomeSection from "./Sections/Home Section/HomeSection.jsx";
-import Guidelines from "./Sections/Guidelines/Guidelines.jsx";
-import EventTimeline from "./Sections/EventTimeline/EventTimeline.jsx";
-import ContactUs from "./Sections/Contact Us/ContactUs.jsx";
-import AwardSection from "./Sections/AwardSection/AwardSection.jsx";
-import Accordition from "./Sections/FAQ/Accordition";
-import FullPageScrolling from "./ReactFullPageScroll.jsx";
-import AboutSection from "./Sections/About Section/AboutSection";
+import FullPageScrolling from "./ReactFullPageScroll";
 
 const App = () => {
   return <FullPageScrolling />;
