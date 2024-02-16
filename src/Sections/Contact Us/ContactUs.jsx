a261a9902d958b0ec1b88e5a64d8d2bf3895e8f6
Verosha Kriyanjala <veroshakriyanjala32@gmail.com>
2024-02-16 14:54:35 +0530

    done

--- src/Sections/Contact Us/ContactUs.jsx
+++ src/Sections/Contact Us/ContactUs.jsx
@@ -3,10 +3,11 @@ import ProfileComponent from "./Contact Us Components/ProfileComponent";
 import fingers from "../../assets/images-contact-us/fingers.svg";
 import notice_board from "../../assets/images-contact-us/notice board.png";
 import notice_board_mobile from "../../assets/images-contact-us/notice-board-mobile.png";
-import janeesha from "../../assets/images-contact-us/janeesha.png";
-import vinu from "../../assets/images-contact-us/vinu.png";
-import methma from "../../assets/images-contact-us/methma.png";
-import sasnika from "../../assets/images-contact-us/sasnika.png";
+import notice_board_tablet from "../../assets/images-contact-us/notice-board-tablet.svg";
+import janeesha from "../../assets/images-contact-us/janeesha.svg";
+import vinu from "../../assets/images-contact-us/vinu.svg";
+import methma from "../../assets/images-contact-us/methma.svg";
+import sasnika from "../../assets/images-contact-us/sasnika.svg";
 import eagle from "../../assets/images-contact-us/eagle sign.svg";
 import "./Contact Us Styles/ContactUs.css";
 
@@ -17,15 +18,24 @@ function ContactUs(props) {
       <p className="contactus-title">CONTACT US</p>
       <div className="shadow"></div>
       <div className="profile-sec">
-        <img src={notice_board} className="notice-board" alt="board"></img>
+        <div className="notice-board-dev">
+          <img src={notice_board} className="notice-board" alt="board"></img>
+        </div>
         <img
           src={notice_board_mobile}
           className="notice-board-mobile"
           alt="board"
         ></img>
+        <div className="notice-board-tablet-dev">
+          <img
+            src={notice_board_tablet}
+            className="notice-board-tablet"
+            alt="board"
+          />
+        </div>
         <img src={eagle} className="eagle" alt="eagle"></img>
         <div className="profile-com-set">
-          <div>
+          <div className="ProfileComponent-dev">
             <ProfileComponent
               profileImg={janeesha}
               first_name={"Janeesha"}
@@ -35,7 +45,7 @@ function ContactUs(props) {
               email={"wickramasinghejj.21@uom.lk"}
             ></ProfileComponent>
           </div>
-          <div>
+          <div className="ProfileComponent-dev">
             <ProfileComponent
               profileImg={vinu}
               first_name={"Vinu"}
@@ -45,7 +55,7 @@ function ContactUs(props) {
               email={"dezoysapvk.21@uom.lk"}
             ></ProfileComponent>
           </div>
-          <div>
+          <div className="ProfileComponent-dev">
             <ProfileComponent
               profileImg={methma}
               first_name={"Methma"}
@@ -55,7 +65,7 @@ function ContactUs(props) {
               email={"weragoda.21@uom.lk"}
             ></ProfileComponent>
           </div>
-          <div>
+          <div className="ProfileComponent-dev">
             <ProfileComponent
               profileImg={sasnika}
               first_name={"Sasnika"}
