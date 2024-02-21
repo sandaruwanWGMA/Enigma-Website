774ffe138de1c0203230a72f9b2fcfb6124e4d8d
RaminduA <raminduanjana22171@gmail.com>
2024-02-21 06:04:26 +0530

    Uploaded Register Now page

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import HomeSection from "./Sections/Home Section/HomeSection";
 import AboutSection from "./Sections/About Section/AboutSection";
 import "./App.css";
 import Guidelines from "./Sections/Guidelines/Guidelines.jsx";
+import RegisterNow from "./Sections/RegisterNow/RegisterNow";
 import EventTimeline from "./Sections/EventTimeline/EventTimeline.jsx";
 
 const App = () => {
   return (
-    <>
+      <>
       <HomeSection></HomeSection>
       <AboutSection></AboutSection>
       <EventTimeline></EventTimeline>
       <Guidelines></Guidelines>
       <Accordition />
-    </>
+      <RegisterNow></RegisterNow>
+      </>
   );
 };
 export default App;
