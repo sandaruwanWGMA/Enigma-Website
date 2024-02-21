88cc48db8022f2fbf3d0af8873b3425e67a9e8fd
RaminduA <raminduanjana22171@gmail.com>
2024-02-21 19:29:34 +0530

    Changed input fields and submit button

--- src/Sections/RegisterNow/RegisterNow.jsx
+++ src/Sections/RegisterNow/RegisterNow.jsx
@@ -3,6 +3,8 @@ import AddMembersIcon from "../../assets/RegiserNow/add_member_icon.png"
 import SeparatorImg from "../../assets/RegiserNow/separator.svg"
 import "../RegisterNow/RegisterNowStyles/RegisterNow.css";
 import React from "react";
+import FullLengthField from "./RegisterNowComponents/FullLengthField";
+import HalfLengthField from "./RegisterNowComponents/HalfLengthField";
 
 
 export const RegisterNow = () => {
@@ -29,26 +31,8 @@ export const RegisterNow = () => {
                             TEAM DETAILS :
                         </div>
                         <div className="relative w-[1223px] h-[230px] top-[59px]">
-                            <div className="absolute w-[1223px] h-[72px] top-0 left-0">
-                                <div className="field-header w-[162px] h-[23px] top-0 left-[24px] [font-family:'Patrick_Hand_SC',Helvetica] text-white text-[20px] absolute font-normal tracking-[0] leading-[40px] whitespace-nowrap">
-                                    TEAM NAME :
-                                </div>
-                                <div className="form-field absolute w-[1223px] h-[38px] top-[34px] left-0 bg-[url(https://c.animaapp.com/xWY8an1h/img/rectangle-24-7.svg)] bg-[100%_100%]">
-                                    <div className="absolute w-[1097px] h-[19px] top-[9px] left-[24px] [font-family:'Patrick_Hand_SC',Helvetica] font-normal text-white text-[14px] tracking-[0] leading-[25px] whitespace-nowrap">
-                                        XTREAM CODERS
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="absolute w-[1223px] h-[72px] top-[79px] left-0">
-                                <div className="field-header w-[231px] h-[23px] top-0 text-[20px] absolute left-[24px] [font-family:'Patrick_Hand_SC',Helvetica] font-normal text-white tracking-[0] leading-[40px] whitespace-nowrap">
-                                    UNIVERSITY OF STUDY :
-                                </div>
-                                <div className="form-field absolute w-[1223px] h-[38px] top-[34px] left-0 bg-[url(https://c.animaapp.com/xWY8an1h/img/rectangle-24-7.svg)] bg-[100%_100%]">
-                                    <div className="w-[1097px] h-[19px] top-[9px] text-[14px] absolute left-[24px] [font-family:'Patrick_Hand_SC',Helvetica] font-normal text-white tracking-[0] leading-[25px] whitespace-nowrap">
-                                        UNIVERSITY OF MORATUWA
-                                    </div>
-                                </div>
-                            </div>
+                            <FullLengthField label="TEAM NAME :" defaultValue="XTREAM CODERS" top="0" />
+                            <FullLengthField label="TEAM EMAIL ADDRESS :" defaultValue="UGDJHSGF@GMAIL.COM" top="79px" />
                         </div>
                     </div>
                 </div>
@@ -58,104 +42,44 @@ export const RegisterNow = () => {
                             TEAM LEADER DETAILS :
                         </div>
                         <div className="relative w-[1223px] h-[230px] top-[59px]">
-                            <div className="absolute w-[607px] h-[72px] top-0 left-0">
-                                <div className="field-header absolute w-[162px] h-[23px] top-0 left-[24px] [font-family:'Patrick_Hand_SC',Helvetica] font-normal text-white text-[20px] tracking-[0] leading-[40px] whitespace-nowrap">
-                                    {" "}
-                                    NAME :
-                                </div>
-                                <div className="form-field absolute w-[603px] h-[38px] top-[34px] left-0 bg-[url(https://c.animaapp.com/xWY8an1h/img/rectangle-24-5.svg)] bg-[100%_100%]">
-                                    <div className="absolute w-[262px] h-[19px] top-[9px] left-[24px] [font-family:'Patrick_Hand_SC',Helvetica] font-normal text-white text-[14px] tracking-[0] leading-[25px] whitespace-nowrap">
-                                        JHCZDJXF KBFJKD
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="absolute w-[607px] h-[72px] top-[79px] left-0">
-                                <div className="field-header absolute w-[162px] h-[23px] top-0 left-[24px] [font-family:'Patrick_Hand_SC',Helvetica] font-normal text-white text-[20px] tracking-[0] leading-[40px] whitespace-nowrap">
-                                    {" "}
-                                    WHATSAPP NUMBER :
-                                </div>
-                                <div className="form-field absolute w-[603px] h-[38px] top-[34px] left-0 bg-[url(https://c.animaapp.com/xWY8an1h/img/rectangle-24-5.svg)] bg-[100%_100%]">
-                                    <div className="absolute w-[262px] h-[19px] top-[9px] left-[24px] [font-family:'Patrick_Hand_SC',Helvetica] font-normal text-white text-[14px] tracking-[0] leading-[25px] whitespace-nowrap">
-                                        JHCZDJXF KBFJKD
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="absolute w-[607px] h-[72px] top-[158px] left-0">
-                                <div className="field-header absolute w-[162px] h-[23px] top-0 left-[24px] [font-family:'Patrick_Hand_SC',Helvetica] font-normal text-white text-[20px] tracking-[0] leading-[40px] whitespace-nowrap">
-                                    {" "}
-                                    NIC :
-                                </div>
-                                <div className="form-field absolute w-[603px] h-[38px] top-[34px] left-0 bg-[url(https://c.animaapp.com/xWY8an1h/img/rectangle-24-5.svg)] bg-[100%_100%]">
-                                    <div className="absolute w-[262px] h-[19px] top-[9px] left-[24px] [font-family:'Patrick_Hand_SC',Helvetica] font-normal text-white text-[14px] tracking-[0] leading-[25px] whitespace-nowrap">
-                                        JHCZDJXF KBFJKD
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="absolute w-[607px] h-[72px] top-0 left-[620px]">
-                                <div className="field-header absolute w-[162px] h-[23px] top-0 left-[24px] [font-family:'Patrick_Hand_SC',Helvetica] font-normal text-white text-[20px] tracking-[0] leading-[40px] whitespace-nowrap">
-                                    {" "}
-                                    YEAR OF STUDY :
-                                </div>
-                                <div className="form-field absolute w-[603px] h-[38px] top-[34px] left-0 bg-[url(https://c.animaapp.com/xWY8an1h/img/rectangle-24-5.svg)] bg-[100%_100%]">
-                                    <div className="absolute w-[262px] h-[19px] top-[9px] left-[24px] [font-family:'Patrick_Hand_SC',Helvetica] font-normal text-white text-[14px] tracking-[0] leading-[25px] whitespace-nowrap">
-                                        SECOND YEAR
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="absolute w-[607px] h-[72px] top-[79px] left-[620px]">
-                                <div className="field-header absolute w-[162px] h-[23px] top-0 left-[24px] [font-family:'Patrick_Hand_SC',Helvetica] font-normal text-white text-[20px] tracking-[0] leading-[40px] whitespace-nowrap">
-                                    {" "}
-                                    EMAIL ADDRESS :
-                                </div>
-                                <div className="form-field absolute w-[603px] h-[38px] top-[34px] left-0 bg-[url(https://c.animaapp.com/xWY8an1h/img/rectangle-24-5.svg)] bg-[100%_100%]">
-                                    <div className="absolute w-[262px] h-[19px] top-[9px] left-[24px] [font-family:'Patrick_Hand_SC',Helvetica] font-normal text-white text-[14px] tracking-[0] leading-[25px] whitespace-nowrap">
-                                        UGDJHSGF@GMAIL.COM
-                                    </div>
-                                </div>
-                            </div>
+                            <HalfLengthField label="NAME :" defaultValue="JHCZDJXF KBFJKD" top="0" left="0" />
+                            <HalfLengthField label="WHATSAPP NUMBER :" defaultValue="JHCZDJXF KBFJKD" top="79px" left="0" />
+                            <HalfLengthField label="UNIVERSITY INDEX / NIC :" defaultValue="JHCZDJXF KBFJKD" top="0" left="620px" />
+                            <HalfLengthField label="EMAIL ADDRESS :" defaultValue="UGDJHSGF@GMAIL.COM" top="79px" left="620px" />
                         </div>
                     </div>
                 </div>
-                <div className="absolute w-[1223px] h-[213px] top-[676px] left-[36px]">
+                <div className="absolute w-[1223px] h-[213px] top-[597px] left-[36px]">
                     <div className="h-[213px]">
                         <div className="form-header absolute w-[300px] h-[48px] top-[8px] left-[5px] [font-family:'Quantico',Helvetica] font-normal text-[#f2b824] text-[23px] text-left tracking-[0] leading-[35px] whitespace-nowrap">
                             TEAM MEMBERS :
                         </div>
                         <div className="relative w-[1223px] h-[72px] top-[59px]">
-                            <div className="absolute w-[1223px] h-[72px] top-0 left-0">
-                                <div className="w-[162px] h-[23px] top-0 left-[24px] [font-family:'Patrick_Hand_SC',Helvetica] text-white text-[20px] absolute font-normal tracking-[0] leading-[40px] whitespace-nowrap">
-                                    NUMBER OF TEAM MEMBERS :
-                                </div>
-                                <div className="form-field absolute w-[1223px] h-[38px] top-[34px] left-0 bg-[url(https://c.animaapp.com/xWY8an1h/img/rectangle-24-7.svg)] bg-[100%_100%]">
-                                    <div className="absolute w-[1097px] h-[19px] top-[9px] left-[24px] [font-family:'Patrick_Hand_SC',Helvetica] font-normal text-white text-[14px] tracking-[0] leading-[25px] whitespace-nowrap">
-                                        XTREAM CODERS
-                                    </div>
-                                </div>
-                            </div>
+                            <FullLengthField label="NUMBER OF TEAM MEMBERS :" defaultValue="XTREAM CODERS" top="0" />
                         </div>
-                        <div className="relative w-[240px] h-[100px] top-[75px] left-0">
+                        <div className="add-member-btn relative w-[200px] h-[50px] top-[75px] left-[-20px]">
                             <img
-                                className="absolute w-[28px] h-[33px] top-[7px] left-[5px]"
+                                className="absolute w-[28px] h-[33px] top-[7px] left-[25px]"
                                 alt="Plus"
                                 src={AddMembersIcon}
                             />
-                            <div className="absolute w-[162px] h-[23px] top-0 left-[48px] [font-family:'Patrick_Hand_SC',Helvetica] text-white text-[20px] font-normal tracking-[0] leading-[48px] whitespace-nowrap">
+                            <div className="absolute w-[162px] h-[23px] top-0 left-[68px] [font-family:'Patrick_Hand_SC',Helvetica] text-white text-[20px] font-normal tracking-[0] leading-[48px] whitespace-nowrap">
                                 ADD MEMBERS
                             </div>
                             <img
-                                className="absolute w-[201px] h-px top-[48px] left-[-20px] object-cover"
+                                className="absolute w-[201px] h-px top-[48px] left-0 object-cover"
                                 alt="Line"
                                 src={SeparatorImg}
                             />
                         </div>
                     </div>
                 </div>
-                <div className="absolute w-[242px] h-[60px] top-[874px] left-[540px]">
+                <div className="absolute w-[242px] h-[60px] top-[795px] left-[540px]">
                     <div className="relative w-[248px] h-[69px] -top-px left-[-4px]">
                         <div className="submit-frame">
                             <button className="submit-btn">
                                 <div className="submit-padding" />
-                                <div className="submit-txt">SUBMIT</div>
+                                <a href="" className="submit-txt">SUBMIT</a>
                             </button>
                         </div>
                     </div>
---
+++ src/Sections/RegisterNow/RegisterNowComponents/FullLengthField.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+function FullLengthField({label, defaultValue, top}) {
+    const [value, setValue] = useState(defaultValue);
+
+    const handleFocus = () => {
+        if (value === defaultValue) {
+            setValue('');
+        }
+    };
+
+    const handleBlur = () => {
+        if (value.trim() === '') {
+            setValue(defaultValue);
+        }
+    };
+
+    return (
+        <div className="absolute w-[1223px] h-[72px] left-0" style={{top: top}}>
+            <div className="field-header w-[162px] h-[23px] top-0 left-[24px] [font-family:'Patrick_Hand_SC',Helvetica] text-white text-[20px] absolute font-normal tracking-[0] leading-[40px] whitespace-nowrap">
+                {label}
+            </div>
+            <div className="form-field absolute w-[1223px] h-[38px] top-[34px] left-0 bg-[url(https://c.animaapp.com/xWY8an1h/img/rectangle-24-7.svg)] bg-[100%_100%]">
+                <input
+                    className="absolute w-[1199px] h-[38px] top-[0] left-[24px] [font-family:'Patrick_Hand_SC',Helvetica] font-normal text-white text-[14px] tracking-[0] leading-[25px] whitespace-nowrap"
+                    type="text"
+                    value={value}
+                    onFocus={handleFocus}
+                    onBlur={handleBlur}
+                    onChange={(e) => setValue(e.target.value)}
+                    style={{ backgroundColor: 'transparent', border: 'none', outline: 'none' }}
+                />
+            </div>
+        </div>
+    );
+}
+
+export default FullLengthField;
