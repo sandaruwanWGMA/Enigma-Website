2c53943a96f05e664e99555da69b11460f380fb9
SandaruwanWGMA <molinduachintha24@gmail.com>
2024-02-25 14:58:08 +0530

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      xs: "480px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
     colors: {
       faqbgcolor: "#4d665e",
       faqcolor: "#facc15",
