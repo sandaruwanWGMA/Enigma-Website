95f6ea74b227f126d806b4c4d5d875c4e863b2e8
SandaruwanWGMA <molinduachintha24@gmail.com>
2024-02-25 15:22:44 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from "react";
-import Accordition from "./Sections/FAQ/Accordition";
-import HomeSection from "./Sections/Home Section/HomeSection";
-import AboutSection from "./Sections/About Section/AboutSection";
-import "./App.css";
-import Guidelines from "./Sections/Guidelines/Guidelines.jsx";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import RegisterNow from "./Sections/RegisterNow/RegisterNow";
-import EventTimeline from "./Sections/EventTimeline/EventTimeline.jsx";
+import HomeSection from "./Sections/Home Section/HomeSection";
+import FullPageScrolling from "./ReactFullPageScroll";
+import ImageComponent from "./Sections/Contact Us/Contact Us Components/ImageComponent";
+import pb from "./assets/images-contact-us/member_board.png";
+import vinu from "./assets/images-contact-us/VINU_.png";
+import ContactUs from "./Sections/Contact Us/ContactUs";
 
 const App = () => {
   return (
-      <>
-      <HomeSection></HomeSection>
-      <AboutSection></AboutSection>
-      <EventTimeline></EventTimeline>
-      <Guidelines></Guidelines>
-      <Accordition />
-      <RegisterNow></RegisterNow>
-      </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<FullPageScrolling />} />
+        <Route path="/RegisterNow" element={<RegisterNow />} />
+      </Routes>
+    </Router>
   );
 };
+
 export default App;
